b661492afffde31546a2d89fce64804b2f135204
ICX\Roman.Kudin <c-rkudin.neogov.net>
2022-05-22 16:35:53 +0300

    Section 19: Publishing (switch to postgres)

--- API/Program.cs
+++ API/Program.cs
@@ -29,7 +29,7 @@ namespace API
             }
             catch (Exception ex)
             {
-                var logger = services.GetRequiredService<ILogger>();
+                var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occurred during migration");
             }
 
